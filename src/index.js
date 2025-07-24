213b8179d50ba4d30a3b988f8d9f97d5aa8e28bc
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-07-24 18:38:21 +0530

    local host fail

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const app = express();
 // }))
 
 const corsOptions = {
-  origin: ["https://loveleetcode.in", "http://localhost:5173"], // replace with your frontend origin
+  origin: "https://loveleetcode.in", // replace with your frontend origin
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
