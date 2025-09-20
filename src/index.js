adabb5832b80805775a656040305a38679debb57
Kevish Sewliya <prokevish07@gmail.com>
2025-09-20 18:17:53 +0530

    cors fix

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const app = express();
 // }))
 
 const corsOptions = {
-  origin: "https://loveleetcode.in/", // replace with your frontend origin
+  origin: "https://loveleetcode.in", // replace with your frontend origin
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
